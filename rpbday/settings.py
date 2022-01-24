aecf06ab0c37d64cc010d73b7a259a0d7b6b242b
Akshay-G-Dev <61789324+Akshay-G-Dev@users.noreply.github.com>
2022-01-24 21:54:06 +0530

    Update settings.py

--- rpbday/settings.py
+++ rpbday/settings.py
@@ -78,11 +78,19 @@ WSGI_APPLICATION = 'rpbday.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'd2ggsip0c3r622',
+
+        'USER': 'jaycgghdjlchhp',
+
+        'PASSWORD': 'b3bdb3f66964029f26120031fe3101af7105143b2e6484e8537d23c46e039f6e',
+
+        'HOST': 'ec2-3-217-216-13.compute-1.amazonaws.com',
+
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
